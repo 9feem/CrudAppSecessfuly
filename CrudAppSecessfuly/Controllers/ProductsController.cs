5222bf7a2764fa111e42ecea00feaf8ec49c0891
LENOVO <LENOVO@DESKTOP-HJVBDOR>
2025-04-09 11:25:21 +0700

    Add project files.

---
+++ CrudAppSecessfuly/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using CrudAppSecessfuly.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudAppSecessfuly.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            return View(_context.products.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Inset()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Inset(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("show");
+            }
+            
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var findedit =  _context.products.Find(id);
+            return View(findedit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.products.Update(product);
+                _context.SaveChanges();
+                return RedirectToAction("Show");
+            }
+            return View(product);
+        }
+
+
+        public IActionResult Delete(int id )
+        {
+            var idname =  _context.products.Find(id);
+            _context.products.Remove(idname);
+            _context.SaveChanges();
+            return RedirectToAction("Show");
+
+        }
+
+    }
+}
---
+++ CrudAppSecessfuly/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudAppSecessfuly.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required(ErrorMessage = "กรุณาระบุชื่อ")]
+        public string Name { get; set; }
+
+
+        //[Range(ErrorMessage ="0000")]
+        [Column(TypeName = "decimal(18,2)")]
+        [Required(ErrorMessage = "กรุณาระบุราคา")]
+        public decimal Price { get; set; }
+
+        [Column(TypeName = "nvarchar(500)")]
+        public string Description { get; set; }
+       
+       
+
+    }
+}
